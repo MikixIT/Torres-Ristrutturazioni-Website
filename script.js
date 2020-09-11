b016d22cbb3a00359baadb3973cbaa21d4f3033d
MikixIT <michael.46@live.it>
2020-09-12 01:35:40 +0200

    Starting GOOD

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburgerbtn = document.getElementById('hamburgerbtn');
+const navbar = document.getElementById('navbar')
+
+hamburgerbtn.addEventListener('click', () => {
+    navbar.classList.toggle('open');
+});    
+
