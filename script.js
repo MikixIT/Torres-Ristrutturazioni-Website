40f0106d02ecc7f14a75d676e93884022571a14b
MikixIT <michael.46@live.it>
2020-09-15 22:37:42 +0200

    New Fix 2.0

--- script.js
+++ script.js
@@ -41,6 +41,57 @@ function next(direction){
  slides[index].classList.add("active");     
 
 }
+var slides=document.querySelector('.slider-items').children;
+ var nextSlide=document.querySelector(".right-slide");
+var prevSlide=document.querySelector(".left-slide");
+var totalSlides=slides.length;
+var index=0;
+
+nextSlide.onclick=function () {
+     next("next");
+     resetTimer();
+}
+prevSlide.onclick=function () {
+     next("prev");
+     resetTimer();
+}
+
+function next(direction){
+
+   if(direction=="next"){
+      index++;
+       if(index==totalSlides){
+        index=0;
+       }
+   } 
+   else{
+           if(index==0){
+            index=totalSlides-1;
+           }
+           else{
+            index--;
+           }
+    }
+
+  for(i=0;i<slides.length;i++){
+          slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");     
+
+}
+
+function resetTimer(){
+ // stop timer after clicked next & prev Button
+ clearInterval(timer);
+ // then start again timer
+ timer = setInterval(autoPlay,5000);
+}
+
+function autoPlay(){
+ next('next');
+}
+
+ var timer = setInterval(autoPlay,5000);
 
 
 
